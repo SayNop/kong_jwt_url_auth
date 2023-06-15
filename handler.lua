beaf4a8997fb3b21fdc4b51362a205316463957e
SayNop <a2045751@163.com>
2023-06-15 18:24:38 +0800

    get token from header

--- handler.lua
+++ handler.lua
@@ -1,13 +1,7 @@
 -- 	main
 local kong = kong
 local pcall = pcall
-
 local get_header = kong.request.get_header
-local set_header = kong.service.request.set_header
-local set_raw_body = kong.service.request.set_raw_body
-local ngx_decode_args = ngx.decode_args
-local encode_args = ngx.encode_args
-local str_find = string.find
 
 
 local RequestAuthHandler = {}
@@ -17,8 +11,14 @@ local RequestAuthHandler = {}
 function local RequestAuthHandler:access(conf)
     local bear_token = get_header("authorization")
     if bear_token == nil then
-        return kong.response.exit(401, { code = 20000, data = "", msg = "Missing required parameters" })
+        return kong.response.exit(401, { code = 401, success = false, data = "", msg = "UnAuthorized" })
     end
+
+    if string.sub(sourcestr, 1, 7) ~= "Bearer " then
+        return kong.response.exit(401, { code = 401, success = false, data = "", msg = "UnAuthorized" })
+    end
+    local token = string.sub(bear_token, 8, -1)
+
 end
 
 -- PRIORITY 越大执行顺序越靠前
